801141296d5abec864987975b58de74aeae66478
kgb33 <github@bassingthwaite.org>
2020-02-26 15:06:51 -0600

    updated docs & added type hints

--- mttools/utils/Profilers.py
+++ mttools/utils/Profilers.py
@@ -1,41 +1,38 @@
 """
 Tools used for profiling tests for efficiency
 """
+from typing import Literal, Callable
 from time import perf_counter_ns
 
 
 class Timer(object):
 
     # TODO: add optional param for number of trials, then average
-    def __init__(self, unit="ms", message=None):
+    def __init__(
+        self, unit: Literal["ns", "us", "ms", "s", "min"] = "ms", message: str = None
+    ):
         """
         Decorator that times the function runtime, then prints the result
         
-        :param unit: (Optional, str)
-            Unit that the time is displayed in, default is ms
+        unit (optional): Unit that the time is displayed in, default is ms
         
-        :param message: (Optional, str)
-            Message that is printed with the elapsed time
+        message (optional): Message that is printed with the elapsed time
         """
         self.unit = unit
         self.message = message
 
-    def __call__(self, f):
+    def __call__(self, f: Callable):
         # TODO: Add a user friendly error message for when the () are forgotten
         """
-        :param f: (func)
-            The function to be timed
+        f: The function to be timed
         """
 
         def wrapper_timer(*args, **kwargs):
-            # times and runs the function
             before = perf_counter_ns()
             rv = f(*args, **kwargs)
             after = perf_counter_ns()
-            # Calculates total time and converts to chosen units
             conversion = self.convert_time()
             time = "{:.3f}".format((after - before) / conversion)
-            # prints output
             print("\n\nTime Elapsed: {0}{1}".format(time, self.unit))
             if self.message is not None:
                 print("\t{}".format(self.message))
@@ -43,7 +40,7 @@ class Timer(object):
 
         return wrapper_timer
 
-    def convert_time(self):
+    def convert_time(self) -> int:
         if self.unit == "ns":
             conversion = pow(10, 0)
         elif self.unit == "us":
