4e02c817aed9f937974553a25817c0a826dab2d5
kgb33 <github@bassingthwaite.org>
2020-02-26 16:16:37 -0600

    Added Real Number and Number

--- mttools/Constants.py
+++ mttools/Constants.py
@@ -1,6 +1,8 @@
-from typing import TypeVar
+from typing import TypeVar, Union
 
 # Custom Types Variables
 Prime = TypeVar("Prime", bound=int)
+RealNumber = Union[int, float]
+Number = Union[RealNumber, complex]
 
 EPSILON = pow(10, -15)
